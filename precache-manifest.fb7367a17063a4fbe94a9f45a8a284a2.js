9001087eb8adb5b547af18fe6a979733f84e3eb2
Sven Cowart <svencowart@gmail.com>
2019-03-18 19:07:01 -0600

    Updates

---
+++ precache-manifest.fb7367a17063a4fbe94a9f45a8a284a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9dd06d80a691a3c965c5",
+    "url": "/nanoslug-generator/static/js/runtime~main.a5f1a131.js"
+  },
+  {
+    "revision": "848da2f9390d6a010fd7",
+    "url": "/nanoslug-generator/static/js/main.f1b4f37e.chunk.js"
+  },
+  {
+    "revision": "d106737eaf106384932e",
+    "url": "/nanoslug-generator/static/js/2.0d684193.chunk.js"
+  },
+  {
+    "revision": "848da2f9390d6a010fd7",
+    "url": "/nanoslug-generator/static/css/main.bd826daf.chunk.css"
+  },
+  {
+    "revision": "d106737eaf106384932e",
+    "url": "/nanoslug-generator/static/css/2.9bc25e65.chunk.css"
+  },
+  {
+    "revision": "6e25bea0f6c7add0c679989d42d2c13b",
+    "url": "/nanoslug-generator/index.html"
+  }
+];
